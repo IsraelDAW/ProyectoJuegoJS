92c87da0d2e5bd991cae1b71b662bbf536505bcf
IsraelDAW <israelantonio.ma44878@cesurformacion.com>
2021-12-21 22:05:02 +0100

    Story 3 con comentarios

--- juego.js
+++ juego.js
@@ -8,26 +8,43 @@ var y = canvas.height-30;
 // Variables dx y dy para marcar la direccion y velocidad de la bola
 var dx = 4;
 var dy = -4;
+// Variable radioBola para indicar el radio (el tamaño) de la bola
+var radioBola = 10;
 
 // Esta funcion dibuja la bola
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, radioBola, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
-// Esta funcion se encarga de la bola no deje un rastro (una linea) a su paso
-// con clearRect() y se modifican los valores de x e y para
-// hacer ese efecto de movimiento de la bola
+// Esta funcion se encarga que la bola no deje
+// un rastro (una linea) a su paso con clearRect()
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+
+    // Con este if se corrige que la bola no desaparezca por la derecha e izquierda
+    // Hace falta usar la variable radioBola porque sino la bola no chocaria,
+    // sino que desapareceria media bola antes de rebotar
+    if(x + dx > canvas.width-radioBola || x + dx < radioBola) {
+        dx = -dx;
+    }
+
+    // Similiar al if de arriba, pero corrrigiendo que no desaparezca tanto por arriba como por abajo
+    // Tambien necesario usar radioBola porque sino rebotaria cuando esté media bola fuera
+    if(y + dy > canvas.height-radioBola || y + dy < radioBola) {
+        dy = -dy;
+    }
+
+    //se modifican los valores de x e y para
+    // hacer ese efecto de movimiento de la bola
     x += dx;
     y += dy;
 }
 
 //con setInterval se dice cada cuanto tiempo
 //se ejecuta la funcion draw()s en milisegundos ( 1 segundo = 1000 )
-setInterval(draw, 100);
+setInterval(draw, 10);
