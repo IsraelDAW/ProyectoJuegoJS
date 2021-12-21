f949e12ca334e3e8ca85e23c44391af4b8ada89d
IsraelDAW <israelantonio.ma44878@cesurformacion.com>
2021-12-21 15:21:03 +0100

    Story 2 con comentario

--- juego.js
+++ juego.js
@@ -1,26 +1,33 @@
 // Variables globales para tomar el canvas
-// Y poder dibujar objetos 2d en el
+// Y poder dibujar objetos 2d en él
 var canvas = document.getElementById("elcanvas");
 var ctx = canvas.getContext("2d");
+// Variables x e y que sirven para decir donde aparecera la bola
+var x = canvas.width/2;
+var y = canvas.height-30;
+// Variables dx y dy para marcar la direccion y velocidad de la bola
+var dx = 4;
+var dy = -4;
 
+// Esta funcion dibuja la bola
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
-// Los 3 siguientes bloques de codigo sirven para crear
-// objectos 2d de distinta forma, color, tamaño...
+// Esta funcion se encarga de la bola no deje un rastro (una linea) a su paso
+// con clearRect() y se modifican los valores de x e y para
+// hacer ese efecto de movimiento de la bola
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+//con setInterval se dice cada cuanto tiempo
+//se ejecuta la funcion draw()s en milisegundos ( 1 segundo = 1000 )
+setInterval(draw, 100);
